49b448b03fc9df084a9f866a932e97e3f3cc88d7
Marianela <mari.ytu@gmail.com>
2014-10-11 17:50:57 -0300

    LookAndFeel

--- Orthodent/src/orthodent/Orthodent.java
+++ Orthodent/src/orthodent/Orthodent.java
@@ -5,6 +5,7 @@
 package orthodent;
 
 import de.javasoft.plaf.synthetica.SyntheticaBlueLightLookAndFeel;
+
 import javax.swing.UIManager;
 
 /**
@@ -17,11 +18,12 @@ public class Orthodent {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*try{
+        
+        try{
             UIManager.setLookAndFeel(new SyntheticaBlueLightLookAndFeel()); // cambia la apariencia de la ventana
         }
         catch(Exception e){
-        }*/
+        }
        
         JVentana ventana = new JVentana();
         new Login(ventana,true).setVisible(true);
