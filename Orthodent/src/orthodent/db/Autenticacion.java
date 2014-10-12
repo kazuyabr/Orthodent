1ce17ffe67914e957c986d8df077509ae2d385ea
msanhuezal <msanhuezal@hotmail.cl>
2014-10-11 22:28:28 -0300

    agregado alg encriptacion md5

--- Orthodent/src/orthodent/db/Autenticacion.java
+++ Orthodent/src/orthodent/db/Autenticacion.java
@@ -4,6 +4,7 @@
  */
 package orthodent.db;
 
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -58,6 +59,25 @@ public class Autenticacion {
         JavaMail mail = new JavaMail();
         mail.send(destino, asunto, mensaje);        
     }
+    
+    public String md5(String clear) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] b = md.digest(clear.getBytes());
+        int size = b.length;
+        StringBuffer h = new StringBuffer(size);
+        //algoritmo y arreglo md5
+            for (int i = 0; i < size; i++) {
+                int u = b[i] & 255;
+                    if (u < 16) {
+                        h.append("0" + Integer.toHexString(u));
+                    }
+                   else {
+                        h.append(Integer.toHexString(u));
+                   }
+               }
+          //clave encriptada
+          return h.toString();
+    }    
     
     static public boolean recuperarContrasena(String email){
         boolean resultado = false;
