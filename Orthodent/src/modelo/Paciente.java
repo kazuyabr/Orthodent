f8cf219f9ef9a271d7a40a84c6037fdf4bb2ec90
msanhuezal <msanhuezal@hotmail.cl>
2014-10-11 23:47:45 -0300

    modelo paciente

--- Orthodent/src/modelo/Paciente.java
+++ Orthodent/src/modelo/Paciente.java
@@ -4,10 +4,95 @@
  */
 package modelo;
 
+import java.util.Date;
+import javax.xml.soap.Text;
+
 /**
  *
  * @author msanhuezal
  */
 public class Paciente {
+    
+    private int id_paciente;
+    private String nombre;
+    private String apellido_pat;
+    private String apellido_mat;
+    private String rut;
+    private Date fechaNacimiento;
+    private Text antecedenteMedico;
+    private String telefono;
+    private boolean activo;    
+
+    public int getId_paciente() {
+        return id_paciente;
+    }
+
+    public void setId_paciente(int id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_pat() {
+        return apellido_pat;
+    }
+
+    public void setApellido_pat(String apellido_pat) {
+        this.apellido_pat = apellido_pat;
+    }
+
+    public String getApellido_mat() {
+        return apellido_mat;
+    }
+
+    public void setApellido_mat(String apellido_mat) {
+        this.apellido_mat = apellido_mat;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Text getAntecedenteMedico() {
+        return antecedenteMedico;
+    }
+
+    public void setAntecedenteMedico(Text antecedenteMedico) {
+        this.antecedenteMedico = antecedenteMedico;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
     
 }
