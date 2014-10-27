74af1420a9918ecb5e3eba0d0f8f5319890dc8ba
msanhuezal <msanhuezal@hotmail.cl>
2014-10-27 16:49:21 -0300

    editar pago db

--- Orthodent/src/orthodent/db/PagoDB.java
+++ Orthodent/src/orthodent/db/PagoDB.java
@@ -6,6 +6,8 @@ package orthodent.db;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import modelo.Pago;
+import modelo.Presupuesto;
 
 /**
  *
@@ -21,6 +23,26 @@ public class PagoDB {
             java.sql.Statement st = con.createStatement();
             int aux = st.executeUpdate("INSERT INTO pago (id_plantratamiento, fecha, abono)\n" +
                                         "VALUES ("+idPlanTratamiento+",'"+fechaCita+"',"+abono+")");
+            boolean resultado = (aux == 1)? true : false;
+            st.close();
+            con.close();
+            return resultado;
+        }
+        catch ( SQLException e) {
+            return false;
+        }
+    }
+    
+    public static boolean editarPago(Pago pago){
+        try{
+            DbConnection db = new DbConnection();
+            Connection con = db.connection;
+            
+            java.sql.Statement st = con.createStatement();
+            int aux = st.executeUpdate("UPDATE pago\n" +
+                                            "SET fecha="+pago.getFecha()+"\n" +
+                                            ",abono="+pago.getAbono()+"\n" +
+                                            "WHERE id_plantratamiento="+pago.getIdPlanTratamiento());
             boolean resultado = (aux == 1)? true : false;
             st.close();
             con.close();
