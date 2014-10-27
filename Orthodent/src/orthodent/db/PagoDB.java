45710ff607bcaa5cfac9f2606dcc0775ceb4a180
msanhuezal <msanhuezal@hotmail.cl>
2014-10-27 16:40:03 -0300

    crear pago db

---
+++ Orthodent/src/orthodent/db/PagoDB.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orthodent.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author msanhuezal
+ */
+public class PagoDB {
+    
+    public static boolean crearPago(int idPlanTratamiento, String fechaCita, String descripcion){
+        try{
+            DbConnection db = new DbConnection();
+            Connection con = db.connection;
+            
+            java.sql.Statement st = con.createStatement();
+            int aux = st.executeUpdate("INSERT INTO pago (id_plantratamiento, fecha_cita, descripcion)\n" +
+                                        "VALUES ("+idPlanTratamiento+",'"+fechaCita+"','"+descripcion+"')");
+            boolean resultado = (aux == 1)? true : false;
+            st.close();
+            con.close();
+            return resultado;
+        }
+        catch ( SQLException e) {
+            return false;
+        }
+    }    
+    
+}
