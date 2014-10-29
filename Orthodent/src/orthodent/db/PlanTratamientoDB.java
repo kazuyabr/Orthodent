487bd4d9d066d02c7e7aff84f324e515ef8cbc1f
msanhuezal <msanhuezal@hotmail.cl>
2014-10-28 22:04:14 -0300

    listar planes de tratamiento de un paciente

--- Orthodent/src/orthodent/db/PlanTratamientoDB.java
+++ Orthodent/src/orthodent/db/PlanTratamientoDB.java
@@ -106,7 +106,34 @@ public class PlanTratamientoDB {
         catch ( SQLException e) {
             return null;
         }
-    }     
+    }    
+    
+    public static ArrayList<PlanTratamiento> listarPlanesTratamientoPaciente(int id_paciente, int id_usuario){
+        ArrayList<PlanTratamiento> planesTratamiento = null;        
+        try {
+            DbConnection db = new DbConnection();
+            Connection con = db.getConnection();
+            
+            java.sql.Statement st = con.createStatement();
+            
+            ResultSet rs = st.executeQuery("SELECT * FROM plan_tratamiento WHERE id_paciente=" + id_paciente + " AND "
+                    + "id_profesional="+id_usuario);
+            planesTratamiento = new ArrayList<PlanTratamiento>();
+            while (rs.next())
+            {
+                PlanTratamiento p = new PlanTratamiento(rs.getInt("id_plantratamiento"), rs.getInt("id_paciente"), rs.getInt("id_profesional"), 
+                                        rs.getString("fecha_creacion_presupuesto"), rs.getString("fecha_modificacion_presupuesto"), rs.getInt("costo_total"),
+                                        rs.getInt("total_abonos"), rs.getInt("avance"), rs.getBoolean("activo"));
+                planesTratamiento.add(p);
+            }
+            rs.close();
+            con.close();
+            return planesTratamiento;
+        }
+        catch ( SQLException e) {
+            return null;
+        }
+    }      
     
     
 }
