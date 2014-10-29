a7294fe747ee47827f7ad0cbbf35dc0da9069f0f
msanhuezal <msanhuezal@hotmail.cl>
2014-10-28 21:36:27 -0300

    crear plan tratamiento db

---
+++ Orthodent/src/orthodent/db/PlanTratamientoDB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orthodent.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author msanhuezal
+ */
+public class PlanTratamientoDB {
+    
+    public static boolean crearPlanTratamiento(int idPaciente, int idProfesional, String fechaCreacionPresupuesto, String fechaModificacionPresupuesto,
+                                               int costoTotal, int totalAbonos, int avance, boolean activo){
+        try{
+            DbConnection db = new DbConnection();
+            Connection con = db.connection;
+            
+            java.sql.Statement st = con.createStatement();
+            int aux = st.executeUpdate("INSERT INTO plan_tratamiento (id_paciente, id_profesional, fecha_creacion_presupuesto, fecha_modificacion_presupuesto, costo_total, total_abonos, avance, activo)\n" +
+                                        "VALUES ("+idPaciente+","+idProfesional+",'"+fechaCreacionPresupuesto+"','"+fechaModificacionPresupuesto+"',"+
+                                                    costoTotal+","+totalAbonos+","+avance+","+activo+")");
+            boolean resultado = (aux == 1)? true : false;
+            st.close();
+            con.close();
+            return resultado;
+        }
+        catch ( SQLException e) {
+            return false;
+        }
+    }    
+    
+    
+}
