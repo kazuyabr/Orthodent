ad1029b8d73f8ddaef15571ace2c6acb40f598ec
msanhuezal <msanhuezal@hotmail.cl>
2014-10-29 00:00:04 -0300

    editar tratamiento pieza plan db

--- Orthodent/src/orthodent/db/TratamientoPiezaPlanDB.java
+++ Orthodent/src/orthodent/db/TratamientoPiezaPlanDB.java
@@ -6,6 +6,7 @@ package orthodent.db;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import modelo.TratamientoPiezaPlan;
 
 /**
  *
@@ -21,6 +22,29 @@ public class TratamientoPiezaPlanDB {
             java.sql.Statement st = con.createStatement();
             int aux = st.executeUpdate("INSERT INTO tratamiento_piezaplan (id_plantratamiento, id_tratamiento, pieza, fecha_realizado, estado)\n" +
                                         "VALUES ("+idPlanTratamiento+","+idTratamiento+","+pieza+",'"+fechaRealizado+"',"+estado+")");
+            boolean resultado = (aux == 1)? true : false;
+            st.close();
+            con.close();
+            return resultado;
+        }
+        catch ( SQLException e) {
+            return false;
+        }
+    }  
+    
+    public static boolean editarTratamientoPiezaPlan(TratamientoPiezaPlan tratamientoPiezaPlan){
+        try{
+            DbConnection db = new DbConnection();
+            Connection con = db.connection;
+            
+            java.sql.Statement st = con.createStatement();
+            int aux = st.executeUpdate("UPDATE tratamiento_piezaplan\n" +
+                                            "SET id_plantratamiento = "+tratamientoPiezaPlan.getIdPlanTratamiento()+"\n" +
+                                            ",id_tratamiento="+tratamientoPiezaPlan.getIdTratamiento()+"\n" +
+                                            ",pieza="+tratamientoPiezaPlan.getPieza()+"\n" +
+                                            "fecha_realizado='"+tratamientoPiezaPlan.getFechaRealizado()+"'\n" +
+                                            ",estado="+tratamientoPiezaPlan.isEstado()+"\n" +
+                                            "WHERE id_tratamiento_piezaplan="+tratamientoPiezaPlan.getIdTratamientoPiezaPlan());
             boolean resultado = (aux == 1)? true : false;
             st.close();
             con.close();
