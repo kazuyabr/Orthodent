f963bd313939af6b4636ecf2d0c4fec932026140
msanhuezal <msanhuezal@hotmail.cl>
2014-10-28 21:50:53 -0300

    editar plan tratamiento db

--- Orthodent/src/orthodent/db/PlanTratamientoDB.java
+++ Orthodent/src/orthodent/db/PlanTratamientoDB.java
@@ -6,6 +6,7 @@ package orthodent.db;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import modelo.PlanTratamiento;
 
 /**
  *
@@ -23,6 +24,32 @@ public class PlanTratamientoDB {
             int aux = st.executeUpdate("INSERT INTO plan_tratamiento (id_paciente, id_profesional, fecha_creacion_presupuesto, fecha_modificacion_presupuesto, costo_total, total_abonos, avance, activo)\n" +
                                         "VALUES ("+idPaciente+","+idProfesional+",'"+fechaCreacionPresupuesto+"','"+fechaModificacionPresupuesto+"',"+
                                                     costoTotal+","+totalAbonos+","+avance+","+activo+")");
+            boolean resultado = (aux == 1)? true : false;
+            st.close();
+            con.close();
+            return resultado;
+        }
+        catch ( SQLException e) {
+            return false;
+        }
+    }
+    
+    public static boolean editarPlanTratamiento(PlanTratamiento planTratamiento){
+        try{
+            DbConnection db = new DbConnection();
+            Connection con = db.connection;
+            
+            java.sql.Statement st = con.createStatement();
+            int aux = st.executeUpdate("UPDATE plan_tratamiento\n" +
+                                            "SET id_paciente="+planTratamiento.getIdPaciente()+"\n" +
+                                            ",id_profesional="+planTratamiento.getIdProfesional()+"\n" +
+                                            ",fecha_creacion_presupuesto='"+planTratamiento.getFechaCreacionPresupuesto()+"'\n" +
+                                            ",fecha_modificacion_presupuesto='"+planTratamiento.getFechaModificacionPresupuesto()+"'\n" +
+                                            ",costo_total="+planTratamiento.getCostoTotal()+"\n" +
+                                            ",total_abonos="+planTratamiento.getTotalAbonos()+"\n" +
+                                            ",avance="+planTratamiento.getAvance()+"\n" +
+                                            ",activo="+planTratamiento.isActivo()+"\n" +
+                                            "WHERE id_plantratamiento="+planTratamiento.getIdPlanTratamiento());
             boolean resultado = (aux == 1)? true : false;
             st.close();
             con.close();
