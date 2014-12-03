9bb2a3cfd2089318c03486cd563cf99b7331d1ec
felipaff <felipaff@gmail.com>
2014-12-03 03:42:05 -0300

    asdqwe
    nefrentefen

--- Orthodent/src/orthodent/bitacora/Bitacoras.java
+++ Orthodent/src/orthodent/bitacora/Bitacoras.java
@@ -5,27 +5,402 @@
  */
 package orthodent.bitacora;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import modelo.Bitacora;
+import modelo.Paciente;
+import modelo.Usuario;
+import orthodent.JVentana;
+import orthodent.db.BitacoraDB;
+import orthodent.db.PacienteDB;
+import orthodent.pacientes.MostrarInfoPaciente;
+import orthodent.pacientes.NuevoPaciente;
 
 /**
  *
  * @author felipe
  */
-public class Bitacoras extends JPanel
-{ 
-  
-    public Bitacoras()
-    {
-         this.setBackground(new Color(243,242,243));
+public class Bitacoras extends JPanel implements ActionListener
+{  
+    private Image bannerFondo;
+    private JTextField buscador;
+    private JButton botonBuscar;
+    private JTable tabla;
+    private TableModel modelo;
+    private Object [][] filas;
+    private String [] columnasNombre;
+    private MostrarInfoPaciente infoPaciente;
+    private JPanel contenedorListarBitacoras;
+    private boolean isListarBitacoras;
+    
+    public Bitacoras(){
+        
+        this.setBackground(new Color(243,242,243));
         this.setPreferredSize(new Dimension(1073, 561));
         
-        JLabel mensaje = new JLabel("Historial... No disponible aún");
-        mensaje.setFont(new Font("Georgia", 0, 11));
-        this.add(mensaje);
+        this.setLayout(new BorderLayout());
+        
+        this.isListarBitacoras = true;
+        this.initComponents();
+        this.addComponents();
+        this.botonBuscar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+     
+    }
+
+    public boolean isIsListarBitacoras() {
+        return isListarBitacoras;
+    }
+
+    public void setIsListarBitacoras(boolean isListarBitacoras) {
+        this.isListarBitacoras = isListarBitacoras;
     }
     
+    public MostrarInfoPaciente getInfoPaciente(){
+        return this.infoPaciente;
+    }
+    
+    private void initComponents(){
+        
+        this.contenedorListarBitacoras = new JPanel();
+        this.contenedorListarBitacoras.setLayout(new BorderLayout());
+        
+        this.infoPaciente = null;
+        this.bannerFondo = new ImageIcon("src/imagenes/directorioHistorial.png").getImage();
+        
+        this.buscador = new JTextField();
+        
+        this.buscador.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent evt) {
+                buscadorKeyTyped(evt);
+            }
+        });
+        
+        this.botonBuscar = new JButton();
+        this.botonBuscar.setForeground(new Color(11, 146, 181));
+        this.botonBuscar.setFont(new Font("Georgia", 1, 12));
+        this.botonBuscar.setIcon(new ImageIcon("src/imagenes/lupa_mini.png"));
+        this.botonBuscar.setText("Buscar");
+        this.botonBuscar.setBorder(null);
+        this.botonBuscar.setBorderPainted(false);
+        this.botonBuscar.setContentAreaFilled(false);
+        this.botonBuscar.addActionListener(this);
+        /*
+        this.nuevoPaciente = new JButton();
+        this.nuevoPaciente.setForeground(new Color(11, 146, 181));
+        this.nuevoPaciente.setFont(new Font("Georgia", 1, 12));
+        this.nuevoPaciente.setIcon(new ImageIcon("src/imagenes/add_mini.png"));
+        this.nuevoPaciente.setText("Nuevo Paciente");
+        this.nuevoPaciente.setBorder(null);
+        this.nuevoPaciente.setBorderPainted(false);
+        this.nuevoPaciente.setContentAreaFilled(false);
+        this.nuevoPaciente.addActionListener(this);
+        */
+        this.tabla = new JTable();
+        this.tabla.setFont(new Font("Georgia", 0, 11));
+        this.columnasNombre = new String [] {"ID", "Accion", "Usuario", "Tabla", "Fecha "};
+        this.updateModelo();
+        this.tabla.getTableHeader().setReorderingAllowed(false);
+        
+        this.tabla.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                JTable table =(JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 2) {
+                    Object [] fila = getRowAt(row);
+                    try {
+                        
+                        //Bitacora bitacora = BitacoraDB.  .getBitacora((String)fila[3]);
+                        Paciente paciente = PacienteDB.getPaciente((String)fila[3]);
+                        
+                        if(paciente!=null){
+                            
+                            //infoPaciente = new MostrarInfoPaciente(paciente, actual);
+                            
+                            remove(contenedorListarBitacoras);
+                            //add(infoPaciente, BorderLayout.CENTER);
+                            isListarBitacoras = false;
+                            updateUI();
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        });
+    }
+    
+    private Object[] getRowAt(int row) {
+        Object[] result = new String[this.columnasNombre.length];
+        
+        for (int i = 0; i < this.columnasNombre.length; i++) {
+            result[i] = this.tabla.getModel().getValueAt(row, i);
+        }
+        
+        return result;
+    }
+    
+    private String girarFecha(String fecha){
+        
+        if(fecha!=null){
+            String año = fecha.substring(0, fecha.indexOf("-"));
+            fecha = fecha.substring(fecha.indexOf("-")+1, fecha.length());
+
+            String mes = fecha.substring(0, fecha.indexOf("-"));
+            fecha = fecha.substring(fecha.indexOf("-")+1, fecha.length());
+
+            String fechaNueva = fecha+"-"+mes+"-"+año;
+            return fechaNueva;
+        }
+        
+        return fecha;
+    }
+    
+    public void updateModelo(){
+        //Podria ser ordenado!! -> una opcion es que la consulta ordene
+        ArrayList<Bitacora> bitacoras = null;
+        
+       /* if(this.actual.getId_rol()==3){
+            //Profesional
+            pacientes = PacienteDB.listarPacientes(this.actual.getId_usuario());
+        }
+        else{
+            pacientes = PacienteDB.listarPacientes();
+        }*/
+        bitacoras = BitacoraDB.listarBitacora();
+        
+        int m = this.columnasNombre.length;
+        
+        ArrayList<Object []> objetos = new ArrayList<Object []>();
+        
+        for(Bitacora bitacora : bitacoras){
+            
+                
+                String fechaNacimiento = this.girarFecha(bitacora.getFecha());
+                
+                Object [] fila = new Object [] {bitacora.getIdBitacora(), bitacora.getAccion(),bitacora.getId_usuario(),
+                                            bitacora.getTabla(), bitacora.getFecha()};
+                
+                objetos.add(fila);
+            
+        }
+        
+        this.filas = new Object [objetos.size()][m];
+        int i = 0;
+        for(Object [] o : objetos){
+            this.filas[i] = o;
+            i++;
+        }
+        
+        this.modelo = new DefaultTableModel(this.filas, this.columnasNombre) {
+            Class[] types = new Class [] {
+                String.class, String.class, String.class, String.class, String.class, String.class, String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        };
+        
+        this.tabla.setModel(modelo);
+    }
+    
+    public void volverPacientes() throws Exception{
+        if(!this.isListarBitacoras){
+            this.remove(this.infoPaciente);
+            this.add(this.contenedorListarBitacoras, BorderLayout.CENTER);
+            this.isListarBitacoras = true;
+            this.updateModelo();
+            this.updateUI();
+        }
+    }
+    
+    private void addComponents(){
+        JPanel auxiliar = new JPanel();
+        auxiliar.setLayout(new BorderLayout());
+        
+        //Inicio Buscador y boton agregar
+        JPanel panel1 = new JPanel();
+        panel1.setPreferredSize(new Dimension(1106, 47));
+        panel1.setBackground(new Color(255,255,255));
+        this.addComponentPanel1(panel1);
+        auxiliar.add(panel1,BorderLayout.NORTH);
+        //Fin Buscador y boton agregar
+        
+        //Inicio Panel azul, con el nombre del Directorio
+        JPanel banner = new JPanel(){
+            @Override
+            public void paint(Graphics g){
+                paintComponent(g);
+                g.drawImage(bannerFondo, 0, 0, 1106, 61, this);
+            }
+        };
+        banner.setBackground(new Color(255,255,255));
+        banner.setPreferredSize(new Dimension(1106, 61));
+        auxiliar.add(banner,BorderLayout.CENTER);
+        //Fin Panel azul, con el nombre del Directorio
+        
+        this.contenedorListarBitacoras.add(auxiliar,BorderLayout.NORTH);
+        
+        //Inicio tabla
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(this.tabla);
+        
+        this.contenedorListarBitacoras.add(scrollPane,BorderLayout.CENTER);
+        //Fin tabla
+        
+        this.add(this.contenedorListarBitacoras,BorderLayout.CENTER);
+    }
+    
+    private void addComponentPanel1(JPanel panel1){
+        
+        GroupLayout groupLayout = new GroupLayout(panel1);
+        panel1.setLayout(groupLayout);
+        groupLayout.setHorizontalGroup(
+            groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(groupLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(this.buscador, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(this.botonBuscar)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                //.addComponent(this.nuevoPaciente)
+                .addContainerGap())
+        );
+        
+        groupLayout.setVerticalGroup(
+            groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(groupLayout.createSequentialGroup()
+                .addContainerGap(15, Short.MAX_VALUE)
+                .addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(this.buscador, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(this.botonBuscar))
+                .addContainerGap(13, Short.MAX_VALUE))
+        );
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+       /* if(e.getSource() == this.nuevoPaciente){
+            new NuevoPaciente(((JVentana)this.getTopLevelAncestor()),true).setVisible(true);
+        }*/
+        
+        if(e.getSource() == this.botonBuscar){
+            this.buscar();
+        }
+    }
+    
+    private void buscadorKeyTyped(KeyEvent evt) {
+        char c = evt.getKeyChar();
+        
+        if(c==KeyEvent.VK_ENTER){
+            evt.consume();
+            
+            if(this.buscador.getText().equals("")){
+                this.updateModelo();
+            }
+            else{
+                this.buscar();
+            }
+        }
+        else if(c==KeyEvent.VK_BACK_SPACE){
+            if(this.buscador.getText().equals("")){
+                this.updateModelo();
+            }
+        }
+    }
+    
+    private void buscar(){
+        String value = this.buscador.getText();
+        
+        ArrayList<Paciente> pacientes = PacienteDB.listarPacientes();
+        
+        int m = this.columnasNombre.length;
+        
+        ArrayList<Object []> objetos = new ArrayList<Object []>();
+        
+        for(Paciente paciente : pacientes){
+            if(paciente.isActivo()){
+                String fechaNacimiento = this.girarFecha(paciente.getFechaNacimiento());
+                
+                Object [] fila = new Object [] {paciente.getNombre(), paciente.getApellido_pat(), paciente.getApellido_mat(),
+                    paciente.getRut(), fechaNacimiento, paciente.getTelefono(), paciente.getAntecedenteMedico()};
+                
+                boolean aux = false;
+                
+                for(Object o : fila){
+                    String obj = (String) o;
+                    obj = obj.toLowerCase();
+                    if(obj.contains(value)){
+                        aux = true;
+                        break;
+                    }
+                }
+                
+                if(aux){
+                    objetos.add(fila);
+                }
+            }
+        }
+        
+        this.filas = new Object [objetos.size()][m];
+        
+        int i = 0;
+        for(Object [] el : objetos){
+            this.filas[i] = el;
+            i++;
+        }
+        
+        this.modelo = new DefaultTableModel(this.filas, this.columnasNombre) {
+            Class[] types = new Class [] {
+                String.class, String.class, String.class, String.class, String.class, String.class, String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        };
+        
+        this.tabla.setModel(modelo);
+    }
+  
 }
