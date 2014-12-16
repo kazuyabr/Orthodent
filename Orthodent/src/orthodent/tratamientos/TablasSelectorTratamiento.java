2f608f56b3efdba508215ce3cb863238f0f3b1c5
msanhuezal <msanhuezal@hotmail.cl>
2014-12-16 00:35:18 -0300

    jDialog selector de tratamientos

---
+++ Orthodent/src/orthodent/tratamientos/TablasSelectorTratamiento.java
@@ -0,0 +1,454 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orthodent.tratamientos;
+
+import java.awt.Dialog;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import modelo.Categoria1;
+import modelo.Categoria2;
+import modelo.Tratamiento;
+import org.springframework.beans.factory.DisposableBean;
+import orthodent.Item;
+import orthodent.db.Categoria1DB;
+import orthodent.db.Categoria2DB;
+import orthodent.db.TratamientoDB;
+
+/**
+ *
+ * @author msanhuezal
+ */
+public class TablasSelectorTratamiento extends javax.swing.JPanel {
+    
+    private TableModel modeloCat1;
+    private Object [][] filasCat1;
+    private String [] columnasNombreCat1;
+    private int categoria1Selected;
+    private TableModel modeloCat2;
+    private Object [][] filasCat2;
+    private String [] columnasNombreCat2;    
+    private int categoria2Selected;
+    private TableModel modeloTratamientos;
+    private Object [][] filasTratamientos;
+    private String [] columnasNombreTratamientos;    
+    private int tratamientoSelected;
+  
+    public TablasSelectorTratamiento() {
+        tratamientoSelected = -1;
+        initComponents();
+        this.iniciarTablaCategoria1();                    
+    }
+        
+    public int getTratamientoSelected() {
+        return tratamientoSelected;
+    }
+
+    public void setTratamientoSelected(int tratamientosSelected) {
+        this.tratamientoSelected = tratamientosSelected;
+    }    
+    
+    public void updateTablas(){
+        this.updateModelo1();
+        if(categoria1Selected > 0){
+          this.updateModelo2(categoria1Selected);
+        }
+        if(categoria2Selected > 0){
+          this.updateModeloTratamientos(categoria2Selected);
+        }
+    }
+    
+    private Object[] getRowAt1(int row) {
+        Object[] result = new Object[this.columnasNombreCat1.length];
+        
+        for (int i = 0; i < this.columnasNombreCat1.length; i++) {
+            result[i] = this.tablaCat1.getModel().getValueAt(row, i);
+        }
+        return result;
+    }
+   
+    private Object[] getRowAt2(int row) {
+        Object[] result = new Object[this.columnasNombreCat2.length];
+        
+        for (int i = 0; i < this.columnasNombreCat2.length; i++) {
+            result[i] = this.tablaCat2.getModel().getValueAt(row, i);
+        }
+        return result;
+    }
+    
+    private Object[] getRowAt3(int row) {
+        Object[] result = new Object[this.columnasNombreTratamientos.length];
+        
+        for (int i = 0; i < this.columnasNombreTratamientos.length; i++) {
+            result[i] = this.tablaTratamientos.getModel().getValueAt(row, i);
+        }
+        return result;
+    }
+   
+    public void updateModelo1(){
+        //Podria ser ordenado!! -> una opcion es que la consulta ordene
+        ArrayList<Categoria1> categorias1 = Categoria1DB.listarCategoria1();
+      
+        int m = this.columnasNombreCat1.length;
+        
+        ArrayList<Object []> objetos = new ArrayList<Object []>();
+        
+        for(Categoria1 categoria1 : categorias1){
+            if(categoria1.isActivo()){
+                Object [] fila = new Object [] {new Item(categoria1.getNombre(), categoria1.getIdCategoria1())};
+                objetos.add(fila);
+            }
+        }
+        
+        this.filasCat1 = new Object [objetos.size()][m];
+        int i = 0;
+        for(Object [] o : objetos){
+            this.filasCat1[i] = o;
+            i++;
+        }
+        
+        this.modeloCat1 = new DefaultTableModel(this.filasCat1, this.columnasNombreCat1) {
+            Class[] types = new Class [] {
+                Item.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        };
+        
+        this.tablaCat1.setModel(modeloCat1);
+    }
+    
+    public void updateModelo2(int idCategoria1){
+        //Podria ser ordenado!! -> una opcion es que la consulta ordene
+        ArrayList<Categoria2> categorias2 = Categoria2DB.listarCategoria2(idCategoria1);
+      
+        int m = this.columnasNombreCat2.length;
+        
+        ArrayList<Object []> objetos = new ArrayList<Object []>();
+        
+        for(Categoria2 categoria2 : categorias2){
+            if(categoria2.isActivo()){
+                Object [] fila = new Object [] {new Item(categoria2.getNombre(), categoria2.getIdCategoria2())};
+                objetos.add(fila);
+            }
+        }
+        
+        this.filasCat2 = new Object [objetos.size()][m];
+        int i = 0;
+        for(Object [] o : objetos){
+            this.filasCat2[i] = o;
+            i++;
+        }
+        
+        this.modeloCat2 = new DefaultTableModel(this.filasCat2, this.columnasNombreCat2) {
+            Class[] types = new Class [] {
+                Item.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        };
+        
+        this.tablaCat2.setModel(modeloCat2);
+    }
+    
+    public void updateModeloTratamientos(int idCategoria2){
+        ArrayList<Tratamiento> tratamientos = new ArrayList<Tratamiento>();
+        
+        if(idCategoria2!=-1){
+            //Podria ser ordenado!! -> una opcion es que la consulta ordene
+            tratamientos = TratamientoDB.listarTratamientosCategoria2(idCategoria2);
+        }
+      
+        int m = this.columnasNombreTratamientos.length;
+        
+        
+        ArrayList<Object []> objetos = new ArrayList<Object []>();
+        
+        for(Tratamiento tratamiento : tratamientos){
+            if(tratamiento.isActivo()){
+                Object [] fila = new Object [] {new Item(tratamiento.getNombre(), tratamiento.getIdTratamiento())};
+                objetos.add(fila);
+            }
+        }
+        
+        this.filasTratamientos = new Object [objetos.size()][m];
+        int i = 0;
+        for(Object [] o : objetos){
+            this.filasTratamientos[i] = o;
+            i++;
+        }
+        
+        this.modeloTratamientos = new DefaultTableModel(this.filasTratamientos, this.columnasNombreTratamientos) {
+            Class[] types = new Class [] {
+                Item.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        };
+        
+        this.tablaTratamientos.setModel(modeloTratamientos);
+    }
+        
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaCat1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tablaCat2 = new javax.swing.JTable();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tablaTratamientos = new javax.swing.JTable();
+        labelCategoria = new javax.swing.JLabel();
+        labelSubCategoria = new javax.swing.JLabel();
+        labelTratamiento = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        tablaCat1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tablaCat1);
+
+        tablaCat2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(tablaCat2);
+
+        tablaTratamientos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane3.setViewportView(tablaTratamientos);
+
+        labelCategoria.setFont(new java.awt.Font("Georgia", 1, 14)); // NOI18N
+        labelCategoria.setForeground(new java.awt.Color(11, 146, 181));
+        labelCategoria.setText("Categoria");
+
+        labelSubCategoria.setFont(new java.awt.Font("Georgia", 1, 14)); // NOI18N
+        labelSubCategoria.setForeground(new java.awt.Color(11, 146, 181));
+        labelSubCategoria.setText("SubCategoria");
+
+        labelTratamiento.setFont(new java.awt.Font("Georgia", 1, 14)); // NOI18N
+        labelTratamiento.setForeground(new java.awt.Color(11, 146, 181));
+        labelTratamiento.setText("Tratamiento");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(labelCategoria)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 277, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 277, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelSubCategoria))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(labelTratamiento)
+                        .addGap(0, 229, Short.MAX_VALUE))
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(32, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(labelSubCategoria)
+                    .addComponent(labelCategoria)
+                    .addComponent(labelTratamiento))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 367, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JLabel labelCategoria;
+    private javax.swing.JLabel labelSubCategoria;
+    private javax.swing.JLabel labelTratamiento;
+    private javax.swing.JTable tablaCat1;
+    private javax.swing.JTable tablaCat2;
+    private javax.swing.JTable tablaTratamientos;
+    // End of variables declaration//GEN-END:variables
+    
+    private void iniciarTablaCategoria1() {
+        this.columnasNombreCat1 = new String [] {"Nombre"};
+        this.updateModelo1();
+        this.tablaCat1.getTableHeader().setReorderingAllowed(false);
+        
+        this.tablaCat1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                JTable table =(JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 1) { 
+                    Object [] fila = getRowAt1(row);
+                    try {
+                        categoria1Selected = ((Item)fila[0]).getId();
+                        iniciarTablaCategoria2();
+                        iniciarTablaTratamientos(-1);
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        });        
+    }
+
+    private void iniciarTablaCategoria2() {
+        this.columnasNombreCat2 = new String [] {"Nombre"};
+        this.updateModelo2(categoria1Selected);
+        this.tablaCat2.getTableHeader().setReorderingAllowed(false);
+        
+        this.tablaCat2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                JTable table =(JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 1) { 
+                    Object [] fila = getRowAt2(row);
+                    try {
+                        categoria2Selected = ((Item)fila[0]).getId();
+                        iniciarTablaTratamientos(categoria2Selected);
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }); 
+    }
+    
+    private void iniciarTablaTratamientos(int aux) {
+        if(aux==-1){
+            this.categoria2Selected = -1;
+        }
+        this.columnasNombreTratamientos = new String [] {"Nombre"};
+        this.updateModeloTratamientos(categoria2Selected);
+        this.tablaTratamientos.getTableHeader().setReorderingAllowed(false);
+        
+        this.tablaTratamientos.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                JTable table =(JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 1) { 
+                    Object [] fila = getRowAt3(row);
+                    try {
+                        tratamientoSelected = ((Item)fila[0]).getId();
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }); 
+    }
+}
