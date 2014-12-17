a9564d417820579b23b292d3f8feab0a7562b34e
Mary Iturriaga <mari.ytu@gmail.com>
2014-12-16 22:26:35 -0300

    :)

--- Orthodent/src/orthodent/pacientes/Recaudacion.java
+++ Orthodent/src/orthodent/pacientes/Recaudacion.java
@@ -740,14 +740,10 @@ public class Recaudacion extends JPanel{
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(labelAbonos)
-
-                    //.addComponent(add, javax.swing.GroupLayout.Alignment.TRAILING))
-
                     .addComponent(add))
-                .addGap(13, 13, 13)
-
+                .addGap(15, 15, 15)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(editAbono)
@@ -766,7 +762,7 @@ public class Recaudacion extends JPanel{
                         .addComponent(editLaboratorio)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(deleteLaboratorio)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)))
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(costoAbono, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(labelAbono, javax.swing.GroupLayout.Alignment.TRAILING))
